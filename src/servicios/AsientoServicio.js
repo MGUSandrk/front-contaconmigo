4549cfde72a14c6554d6f0e1b8430d56342c0c3f
Mirko Sandrk <110252372+MGUSandrk@users.noreply.github.com>
2025-11-14 16:31:36 -0300

    Update AsientoServicio.js

--- src/servicios/AsientoServicio.js
+++ src/servicios/AsientoServicio.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
-const ASIENTO_BASE_REST_API_URL = API_BASE_URL+"/entry';
+const ASIENTO_BASE_REST_API_URL = API_BASE_URL+'/entry';
 
 class AsientoServicio {
 
