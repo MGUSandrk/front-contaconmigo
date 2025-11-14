ae15261873eb2cbbfc69280e0c0dc3ee6167db4c
Mirko Sandrk <110252372+MGUSandrk@users.noreply.github.com>
2025-11-14 16:28:29 -0300

    Update UsuarioServicio.js

--- src/servicios/UsuarioServicio.js
+++ src/servicios/UsuarioServicio.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL 
-const USUARIO_BASE_REST_API_URL = "${API_BASE_URL}/user";
+const USUARIO_BASE_REST_API_URL = API_BASE_URL+"/user";
 
 const config = {
     headers: {
